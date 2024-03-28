7eb40b25f24f5c03374a17f2d44d1a8a8a9dd98f
kalelkar-noman <kalelkarnoman014@gmail.com>
2024-03-28 23:40:44 +0530

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+document.getElementById("nav-open").addEventListener("click", () => {
+  document.getElementById("nav").classList.toggle("nav-active");
+});
+
+document.getElementById("nav-close").addEventListener("click", () => {
+  document.getElementById("nav").classList.toggle("nav-active");
+});
